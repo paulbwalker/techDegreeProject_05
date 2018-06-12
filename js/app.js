95e01bf335ba75d3eeeb3385b41997f7c936e348
Paul Walker <pw97960@gmail.com>
2018-06-12 16:27:22 -0700

    Update style.css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log("Hello from app.js");
+$(function(){
+  $('a[rel=lightbox]').lightBox({
+    containerResizeSpeed: 250,
+    fixedNavigation: true
+  });
+  $('a[rel=2ndlightbox]').lightBox({
+    overlayBgColor: '#fff',
+    overlayOpacity: 0.7
+  });
+});

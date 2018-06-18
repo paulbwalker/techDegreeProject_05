7cdebd298f073431145525486b54e491eb6862d9
Paul Walker <pw97960@gmail.com>
2018-06-18 04:35:48 -0700

    Add Fetch API

--- js/app.js
+++ js/app.js
@@ -1,59 +1,72 @@
-const select = document.querySelector('.employee-list');
+const card = document.querySelectorAll('.card');
+const cardContent = document.querySelector('.card-content');
+const modalEm = document.querySelector('.modal-employee');
+const myModal = document.getElementById('myModal');
+
+
 
 /* FETCH FUNCTIONS ----------------------------------------------- */
 
+// This function assign the .then(response => response.json())
 function fetchData(url) {
 	return fetch(url)
 		.then(res => res.json())
 }
 
 fetchData('https://randomuser.me/api/?nat=us,gb&results=12')
-	.then(data => console.log(data.results))
-	// 	// generateEmployees(data.results))
-	// 	console.log(data.results))
-
-
-
+	.then(data => generateResults(data.results))
+	// .then(data => console.log(data.results))	
+	
 
 
 
 /* HELPER FUNCTIONS ---------------------------------------------- */
 
+	let employee = [];
+	function generateResults(data) {
+		data.forEach(function (results) {
+			employee += `
+				<div class="card">
+					<img class="avatar" src="${ results.picture.large }" alt="Avatar">
+					<div class="container">
+						<h4 class="name">${ results.name.first}  ${ results.name.last }</h4>
+						<p class="email">${ results.email }</p>
+						<p class="city">${ results.location.city }</p>
+					</div>
+				</div>
+			`;
+		});
+		cardContent.innerHTML = employee;
+	}
 
-	// const generateEmployees = (data) => {
-	// 	const employees = data.map(item => `
-	// 		<li class="employee-item">
-	// 			<div class="container">
-	// 				<h4>${ name }</h4>					
-	// 				<p>${ email }</p>
-	// 				<p>${ city }<p>
-	// 			</div>
-	// 		</li>
-	// 	`);
-	// 	select.innerHTML = employees;
-	// }
-
-
-
-
-
-
-
-/* EVENT LISTENERS ----------------------------------------------- */
-
+	function generateModal(data) {
+		const modalContent = data.forEach(results => `
+				<div class="modal-content">
+					<span class="close">&times;</span>
+					<div class="card">
+						<img class="avatar" src="${ results.picture.large }" alt="Avatar">
+						<div class="container">
+							<h4 class="name">${ results.name.first}  ${ results.name.last }</h4>
+							<p class="email">${ results.email }</p>
+							<p class="city">${ results.location.city }</p>
+						</div>
+					</div>
+					<div class="modal-more-info container">
+						<h4 class="Phone">${ results.phone }</h4>
+						<p class="address">${ results.address }</p>
+						<p class="dob">${ results.dob }</p>
+					</div> 
+				</div>
+			`);
+			myModal.innerHTML = modalContent;
+		}
 
 /* MODAL --------------------------------------------------------- */
 
-const modalCard = document.querySelectorAll('.card');
+
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
- 	for (let i = 0; i < modalCard.length; i++) {
- 		modalCard[i].style.cursor = 'pointer';
- 		modalCard[i].addEventListener('click', function(event) {
-			modal.style.display = "block";
- 		});
- 	}
 
 // Get the modal
 var modal = document.getElementById('myModal');
@@ -64,9 +77,9 @@ var btn = document.getElementById("myBtn");
 
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {

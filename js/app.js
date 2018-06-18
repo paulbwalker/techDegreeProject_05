a92f34d4cd993fa280cdb4c972ce3226a757b549
Paul Walker <pw97960@gmail.com>
2018-06-18 06:46:15 -0700

    Use Map instead of forEach

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 const card = document.querySelectorAll('.card');
 const cardContent = document.querySelector('.card-content');
-const modalEm = document.querySelector('.modal-employee');
-const myModal = document.getElementById('myModal');
-
+const modal = document.querySelectorAll('.modal-content');
 
 
 /* FETCH FUNCTIONS ----------------------------------------------- */
@@ -13,77 +11,32 @@ function fetchData(url) {
 		.then(res => res.json())
 }
 
+// You call the function fetchData to call .then(res => res.json) then chain data method
 fetchData('https://randomuser.me/api/?nat=us,gb&results=12')
 	.then(data => generateResults(data.results))
-	// .then(data => console.log(data.results))	
+
 	
-
-
 
 /* HELPER FUNCTIONS ---------------------------------------------- */
 
-	let employee = [];
 	function generateResults(data) {
-		data.forEach(function (results) {
-			employee += `
-				<div class="card">
-					<img class="avatar" src="${ results.picture.large }" alt="Avatar">
-					<div class="container">
-						<h4 class="name">${ results.name.first}  ${ results.name.last }</h4>
-						<p class="email">${ results.email }</p>
-						<p class="city">${ results.location.city }</p>
-					</div>
-				</div>
-			`;
-		});
-		cardContent.innerHTML = employee;
-	}
-
-	function generateModal(data) {
-		const modalContent = data.forEach(results => `
-				<div class="modal-content">
-					<span class="close">&times;</span>
+		const employees = data.map(item => `
 					<div class="card">
-						<img class="avatar" src="${ results.picture.large }" alt="Avatar">
+						<img class="avatar" src="${ item.picture.large }" alt="Avatar">
 						<div class="container">
-							<h4 class="name">${ results.name.first}  ${ results.name.last }</h4>
-							<p class="email">${ results.email }</p>
-							<p class="city">${ results.location.city }</p>
+							<h4 class="name">${ item.name.first}  ${ item.name.last }</h4>
+							<p class="email">${ item.email }</p>
+							<p class="city">${ item.location.city }</p>
 						</div>
 					</div>
-					<div class="modal-more-info container">
-						<h4 class="Phone">${ results.phone }</h4>
-						<p class="address">${ results.address }</p>
-						<p class="dob">${ results.dob }</p>
-					</div> 
-				</div>
-			`);
-			myModal.innerHTML = modalContent;
+					<div class="modal modal-more-info container">
+							<h4 class="Phone">${ item.phone }</h4>
+							<p class="address">${ item.address }</p>
+							<p class="dob">${ item.dob }</p>
+						</div> 
+					</div>
+			`).join('');
+			cardContent.innerHTML = employees;
 		}
 
-/* MODAL --------------------------------------------------------- */
 
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}

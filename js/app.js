72462e4636a7be2f56415fade4c6f81cbb47666a
Paul Walker <pw97960@gmail.com>
2018-08-23 09:14:53 -0700

    upDate files

--- js/app.js
+++ js/app.js
@@ -1,40 +1,86 @@
 'use strict';
 
 const cardContent = document.querySelector('.card-content');
-const card = document.querySelectorAll('.card');
-const modalDisplay = document.querySelector('.modal-display');
 const myModal = document.getElementById('myModal');
 const modalInfo = document.getElementsByClassName('modal-info');
 const modal = document.getElementsByClassName('modal');
+const modalDisplay = document.querySelector('.modal-display');
+const header = document.querySelector('.masthead');
+
+
+
+
+// Simple display function add class and removes class to hide/show modal. el is shorthand for element.
+const showModalFunc = (el) => {
+  el.classList.remove('is-hidden');
+  el.classList.add('is-flex-display');
+}
+
+const removeModalFunc = (el) => {
+  el.classList.remove('is-flex-display');
+  el.classList.add('is-hidden');
+}
+
+
+/* I USED THIS PLACEHOLDER TO BUILD THE SEARCH FORM ------------ */
+
+// <form class="search-form">
+//   <input class="search" for="search" type="search" onkeyup="filterEmployee()" placeholder="Search..">
+//   <button class="search-btn" onkeyup="clearFilter()" type="reset">Search</button>
+// </form>
+
+const form = document.createElement('form');
+header.appendChild(form);
+form.classList.add('search-form');
+const formInput = document.createElement('input');
+formInput.type = 'search';
+formInput.classList.add('search');
+formInput.setAttribute('onkeyup', 'filterEmployee()');
+formInput.placeholder = 'Enter name or username';
+form.appendChild(formInput);
+const button = document.createElement('button');
+form.appendChild(button);
+button.classList.add('search-btn');
+button.setAttribute('onkeyup', 'clearFilter()');
+button.type = 'reset';
+button.textContent = 'filter';
+    
+const para = document.createElement('p');
+
+
+
+/* END OF FORM ---------------------------------------------------- */
+
+
 
 /* FETCH FUNCTIONS ----------------------------------------------- */
 
 // This function assign the .then(response => response.json())
 function fetchData(url) {
-	return fetch(url)
-		.then(res => res.json())
+  return fetch(url)
+    .then(res => res.json())
 }
 
 
 /* HELPER FUNCTIONS ---------------------------------------------- */
 
 
-	(function generateResults() {
+  (function generateResults() {
 
     // You call the function fetchData to call .then(res => res.json) then chain data method
     fetchData('https://randomuser.me/api/?nat=us,gb&results=12')
 
-    // .then(data => console.log(data.results))	
+    // .then(data => console.log(data.results)) 
     .then(data => {
       const listEmployee = data.results;
-      data.results.map(results => {
+      data.results.map(employees => {
         cardContent.innerHTML += `
                             <div class="card">
-                                <img class="avatar" src="${ results.picture.large }" alt="Avatar">
+                                <img class="avatar" src="${ employees.picture.large }" alt="Avatar">
                                 <div class="container">
-                                  <h4 class="name">${ results.name.first}  ${ results.name.last }</h4>
-                                  <p class="email">${ results.email }</p>
-                                  <p class="city">${ results.location.city }</p>
+                                  <h4 class="name">${ employees.name.first}  ${ employees.name.last }</h4>
+                                  <p class="email">${ employees.email }</p>
+                                  <p class="city">${ employees.location.city }</p>
                                 </div>
                             </div>
                             `;
@@ -42,64 +88,108 @@ function fetchData(url) {
       const card = document.querySelectorAll('.card');
       employeeDetail(listEmployee, card);
   })
-  })();  
+})();  
 
   
   /* MODAL --------------------------------------------------------- */
 
+
 // Pass through generateResults add eventListener to get modal
 
-function employeeDetail ( listEmployee, card) {
-  const generateModal = listEmployee.map(items =>  `
-      <div id="myModal" class="modal">
+const employeeDetail = ( listEmployee, card) => {
+  const modalDisplay = document.querySelector('.modal-display');
+  const modal = document.querySelectorAll('.modal');
+  const generateModal = listEmployee.map(employees =>  `
+      <div class="modal is-hidden">
         <div class="modal-content">        
           <div class="modal-info">
             <span class="close">&times;</span>
-                <img src="${ items.picture.large }" class="modal-avatar cf" alt="Avatar">
+                <img src="${ employees.picture.large }" class="modal-avatar cf" alt="Avatar">
                 <div class="modal-text">
-                  <h4 class="name">${ items.name.first}  ${ items.name.last }</h4>
-                  <p class="email">${ items.email }</p>
-                  <p class="city">${ items.location.city }</p>
+                  <h4 class="fullname">${ employees.name.first}  ${ employees.name.last }</h4>
+                  <p class="email">${ employees.email }</p>
+                  <p class="city">${ employees.location.city }</p>
                 </div>
                 <div class="arrow cf">
-                  <span class="prev">&lt;</span>
-                  <span class="next">&gt;</span>
+                  <span class="prev">&#10094;</span>
+                  <span class="next">&#10095;</span>
                 </div> 
                 <div class="modal-more-info">
-                  <h4 class="cell">${ items.cell }</h4>
-                  <p class="address">${ items.location.street }, ${ items.location.state } ${ items.location.postcode }
+                  <h4 class="cell">${ employees.cell }</h4>
+                  <p class="address">${ employees.location.street }, 
+                                     ${ employees.location.state } 
+                                    ${ employees.location.postcode }
                   </p>
-                  <p class="dob">Birthday: ${ items.dob.date }</p>
+                  <p class="dob">Birthday: ${ employees.dob.date.slice(0, 10) }</p>
                 </div>
             </div>
         </div>
       </div> 
     `).join('');
-    const modal = document.querySelectorAll('.modal');
     modalDisplay.innerHTML = generateModal;
-    modalDetails(card);
+    modalDetails(card, listEmployee);
+  }
+
+/* FILTER THROUGH EMPLOYEES ---------------------------------------------------------- */
+
+  // Add eventListener to the form field and preventDefault to submit form
+  function filterEmployee() {
+    // Declare variables
+    const input = document.querySelector('input');
+    const filter = input.value.toUpperCase();
+    let name = document.querySelectorAll('.name');
+    const heading4 = document.querySelectorAll('h4')
+    const card = document.querySelectorAll('.card');
+    const btn = document.querySelector('button');
+
+
+    // Loop through all employees name, and hide those who don't match the search query
+    for (let i = 0; i < card.length; i++) {
+      if (name[i].textContent.toUpperCase().includes(filter)) {
+        showModalFunc(card[i]);
+      } else {
+        removeModalFunc(card[i]);
+      }
+      const clearFilter = (e) => {
+        e.preventDefault();
+        input.value = '';
+      }
+    }
   }
 
 
 
-function modalDetails (card) {
-  let modalCollection = [];
+/* MODAL DISPLAY & UI ---------------------------------------------------------------------*/
+
+function modalDetails (card, listEmployee) {
   for (let i = 0; i < card.length; i++) { 
-    modalCollection += card;
     const closeX = modal[i].querySelector('.close');
-    const ltMark = modal[i].querySelector('.prev');  
+    const ltMark = modal[i].querySelector('.prev'); 
+    const gtMark = modal[i].querySelector('.next');
     card[i].addEventListener('click', () => {
-      modal[i].style.display = 'block';
-    }); 
-    closeX.addEventListener('click', () => { 
-      modal[i].style.display = 'none';     
-    }); 
-    ltMark.addEventListener('click', (e) => { 
-      for(let i = 0; i < modal.length; i++) {
-        console.log(modal[i]);
-      }
+      showModalFunc(modal[i]);
+    });
+    closeX.addEventListener('click', () => {
+      removeModalFunc(modal[i]);
+    });
+  
+    ltMark.addEventListener('click', () => {
+        if(modal[i].className !== 'is-hidden') {
+         window.setTimeout(() => {
+          removeModalFunc(modal[i]);
+         }, 500);
+          showModalFunc(modal[i].previousElementSibling);
+        }
+    });
+
+    gtMark.addEventListener('click', () => {
+        if(modal[i].className !== 'is-hidden') {
+         window.setTimeout(() => {
+          removeModalFunc(modal[i]);
+         }, 500);
+          showModalFunc(modal[i].nextElementSibling);
+        }
     });
   }
+
 }
-
-
